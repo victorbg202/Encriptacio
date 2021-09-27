f97722f2118836523557ee238798c60cfd9b49b3
gabrigomezayuso <ggomezayuso@gmail.com>
2021-09-27 17:00:52 +0200

    cambios

--- M09-PT1 (Encriptacio)/CRYPT/src/Principal.java
+++ M09-PT1 (Encriptacio)/CRYPT/src/Principal.java
@@ -3,8 +3,8 @@ public class Principal extends Crypt {
 public static void main(String[] args) {
 		//Variables
 		String comando;
-        int estat;
-
+        int estat = 0;
+		IntroduirEstat(estat);
         //Tancar teclat
         sc.close();
     }
@@ -13,16 +13,39 @@ public static void main(String[] args) {
 	public static int IntroduirEstat(int estat) {
         do {
         	//Introduiir estat
-        	
+			OpcionesMenu();
+			estat=sc.nextInt();
         	
 			//entar comanda
         	switch (estat) {
 				case 0:
+					System.out.println("Ha sido un placer!");
+					break;
 				case 1:
+					entrarMissatge();
+									
+					break;
 			}
+
 		} while (estat!=0);
-        
+        while (estat!=1);
 		return estat;
+	}
+
+	public static void OpcionesMenu(){
+		System.out.println(" \t\t ----------------------------------");
+		System.out.println("\t\t||"+"*****"+"||"+"  MENU PRINCIPAL  "+"||"+"*****"+"||");
+		System.out.println("\t\t ----------------------------------");
+		System.out.println("\t\t||                                ||");
+		System.out.println("\t\t||    "+"************************"+"    ||");
+		System.out.println("\t\t||  //                        \\\\  ||");
+		System.out.println("\t\t|| ||        "+"1) INICIAR"+"        || ||");
+		System.out.println("\t\t|| ||        "+"0) SALIR"+"          || ||");
+		System.out.println("\t\t|| ||                          || ||");
+		System.out.println("\t\t||   "+"**************************"+"   ||");
+		System.out.println("\t\t||                                ||");
+		System.out.println("\t\t ----------------------------------");
+		System.out.print("Introduce que opción vas a escojer:");
 	}
     
 }
